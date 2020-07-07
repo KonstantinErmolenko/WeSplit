403196d463a0299103954ac5061128cb603eeb00
ermolenko_ks <19symbols@gmail.com>
2020-07-07 19:25:34 +0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ермоленко Константин on 07.07.2020.
+//  Copyright © 2020 Ермоленко Константин. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
